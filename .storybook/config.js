3d500880e22faa51489c0d0d567dcb40b7c92109
Dave Rau <dave.rau@optimizely.com>
2020-05-27 14:02:30 -0700

    color story

--- .storybook/config.js
+++ .storybook/config.js
@@ -30,6 +30,7 @@ const req = require.context('../src/', true, /story\.js$/);
 function loadStories() {
   require('./overview.story.js');
   require('./tokens.story.js');
+  require('./color.story.js');
   require('./css.story.js');
   require('./borderradius.story.js');
   req.keys().forEach(req);
