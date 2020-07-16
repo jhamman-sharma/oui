0ff4cfd4ee41f26ddc27d0d20c51e227b5fc13e8
Emily Lepert <elepert@olin.edu>
2020-07-16 16:30:04 -0700

    added col

--- src/components/Form/Form.story.tsx
+++ src/components/Form/Form.story.tsx
@@ -5,6 +5,7 @@ import { storiesOf } from '@storybook/react';
 import Form from './index';
 import Input from '../Input';
 import DatePicker from '../DatePicker/DatePicker.js';
+import Col from '../Layout/Col';
 
 const stories = storiesOf('Form', module);
 stories
@@ -14,43 +15,65 @@ stories
   .add(
     'Default', (() => (
       <div className="push-double--top">
-        <Form title="Form title" description="This is the form description">
-          <Form.Section isRequired={true} title="Form section">
-            <Form.Item>
-              <Input
-                label="Input label"
-                note="A short description or note about this field."
-                placeholder="Just a placeholder"
-                type="text"
-                id="input-01"
-              />
-            </Form.Item>
-            <Form.Row>
-              <Form.Item>
-                <DatePicker
-                  inputId="date-picker-id-1"
-                  initialDate={ null }
-                  isFocused={ false }
-                />
-                <p>I appear after the calendar</p>
-              </Form.Item>
-              <Form.Item>
-                <DatePicker
-                  inputId="date-picker-id-2"
-                  initialDate={ null }
-                  isFocused={ false }
-                />
-                <p>I appear after the calendar</p>
-              </Form.Item>
-            </Form.Row>
-            <Form.Row fillWidth={true}>
+        <Col small={ 7 } large={ 7 }>
+          <Form title="Form title" description="This is the form description">
+            <Form.Section isRequired={true} title="Form section">
               <Form.Item>
                 <Input
                   label="Input label"
                   note="A short description or note about this field."
                   placeholder="Just a placeholder"
                   type="text"
-                  id="input-02"
+                  id="input-01"
+                />
+              </Form.Item>
+              <Form.Row>
+                <Form.Item>
+                  <DatePicker
+                    inputId="date-picker-id-1"
+                    initialDate={ null }
+                    isFocused={ false }
+                  />
+                  <p>I appear after the calendar</p>
+                </Form.Item>
+                <Form.Item>
+                  <DatePicker
+                    inputId="date-picker-id-2"
+                    initialDate={ null }
+                    isFocused={ false }
+                  />
+                  <p>I appear after the calendar</p>
+                </Form.Item>
+              </Form.Row>
+              <Form.Row fillWidth={true}>
+                <Form.Item>
+                  <Input
+                    label="Input label"
+                    note="A short description or note about this field."
+                    placeholder="Just a placeholder"
+                    type="text"
+                    id="input-02"
+                  />
+                </Form.Item>
+                <Form.Item>
+                  <Input
+                    label="Input label"
+                    note="A short description or note about this field."
+                    placeholder="Just a placeholder"
+                    type="text"
+                    id="input-03"
+                  />
+                </Form.Item>
+              </Form.Row>
+            </Form.Section>
+            <Form.Section isOptional={true} helpIcon={true} popoverText="This is some helpful text" title="Form section" description="This is the section description">
+              <Form.Item>
+                <Input
+                  label="Input label"
+                  note="A short description or note about this field."
+                  placeholder="Just a placeholder"
+                  type="text"
+                  id="input-04"
                 />
               </Form.Item>
               <Form.Item>
@@ -59,78 +82,60 @@ stories
                   note="A short description or note about this field."
                   placeholder="Just a placeholder"
                   type="text"
-                  id="input-03"
+                  id="input-05"
                 />
               </Form.Item>
-            </Form.Row>
-          </Form.Section>
-          <Form.Section isOptional={true} helpIcon={true} popoverText="This is some helpful text" title="Form section" description="This is the section description">
-            <Form.Item>
-              <Input
-                label="Input label"
-                note="A short description or note about this field."
-                placeholder="Just a placeholder"
-                type="text"
-                id="input-04"
-              />
-            </Form.Item>
-            <Form.Item>
-              <Input
-                label="Input label"
-                note="A short description or note about this field."
-                placeholder="Just a placeholder"
-                type="text"
-                id="input-05"
-              />
-            </Form.Item>
-          </Form.Section>
-          <Form.Section title="Form section" description="This is the section description">
-            <Form.Item>
-              <Input
-                label="Input label"
-                note="A short description or note about this field."
-                placeholder="Just a placeholder"
-                type="text"
-                id="input-06"
-              />
-            </Form.Item>
-          </Form.Section>
-        </Form>
+            </Form.Section>
+            <Form.Section title="Form section" description="This is the section description">
+              <Form.Item>
+                <Input
+                  label="Input label"
+                  note="A short description or note about this field."
+                  placeholder="Just a placeholder"
+                  type="text"
+                  id="input-06"
+                />
+              </Form.Item>
+            </Form.Section>
+          </Form>
+        </Col>
       </div>
     ))
   )
   .add(
     'Replicated "New A/B Test" form', (() => (
       <div className="push-double--top">
-        <Form>
-          <Form.Section title="Experiment Name">
-            <Form.Item>
-              <Input
-                label="Add a name for your experiment"
-                type="text"
-                id="input-01"
-              />
-            </Form.Item>
-          </Form.Section>
-          <Form.Section title="Experiment Key">
-            <Form.Item>
-              <Input
-                label="Use this to uniquely identify the experiment"
-                type="text"
-                id="input-02"
-              />
-            </Form.Item>
-          </Form.Section>
-          <Form.Section isOptional={true} title="Description">
-            <Form.Item>
-              <Input
-                label="What is your experiment? State your hypothesis and notes here."
-                type="text"
-                id="input-03"
-              />
-            </Form.Item>
-          </Form.Section>
-        </Form>
+        <Col small={ 7 } large={ 7 }>
+          <Form>
+            <Form.Section title="Experiment Name">
+              <Form.Item>
+                <Input
+                  label="Add a name for your experiment"
+                  type="text"
+                  id="input-01"
+                />
+              </Form.Item>
+            </Form.Section>
+            <Form.Section title="Experiment Key">
+              <Form.Item>
+                <Input
+                  label="Use this to uniquely identify the experiment"
+                  type="text"
+                  id="input-02"
+                />
+              </Form.Item>
+            </Form.Section>
+            <Form.Section isOptional={true} title="Description">
+              <Form.Item>
+                <Input
+                  label="What is your experiment? State your hypothesis and notes here."
+                  type="text"
+                  id="input-03"
+                />
+              </Form.Item>
+            </Form.Section>
+          </Form>
+        </Col>
       </div>
     ))
   );
